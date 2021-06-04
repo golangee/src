4189473dd850695227c59e3c3f0f4ac3677d5f5a
Torben Schinke <torben.schinke@worldiety.de>
2021-06-04 09:16:51 +0200

    allowing module root package definition

--- golang/rmod.go
+++ golang/rmod.go
@@ -21,7 +21,7 @@ func (r *Renderer) renderMod(mod *ast.Mod, parent *render.Dir) (*render.Dir, err
 
 	for _, pkg := range mod.Pkgs {
 		// we cannot use name here, because in go the name and the import path may be different
-		if !strings.HasPrefix(pkg.Path, mod.Name+"/") {
+		if !strings.HasPrefix(pkg.Path, mod.Name) {
 			return nil, fmt.Errorf("declared package '%s' must be prefixed by module path '%s'", pkg.Path, mod.Name)
 		}
 
